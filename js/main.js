0888725de4df4832c68beb5a37871e951d8f858a
Moe Ko <moethantkoko@gmail.com>
2022-04-25 12:47:19 -0400

    added a cow photo and edit some words

--- js/main.js
+++ js/main.js
@@ -1,92 +1,75 @@
-
 function getFetch(){
-  let inputVal = document.getElementById('barcode').value
-
-  if(inputVal.length !== 12){
-    alert('Please ensure that barcode is 12 characters')
-    return;
+  let inputVal = document.getElementById("barcode").value;
+  console.log(inputVal.length)
+  if (inputVal.length !== 12) {
+      alert(`Please ensure that barcode length is 12 characters.`)
+      return;
   }
-
   const url = `https://world.openfoodfacts.org/api/v0/product/${inputVal}.json`
-
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        if(data.status === 1){
-        const item = new ProductInfo(data.product)
-        console.log(item.ingredients)
-        item.testCall()
-        item.showInfo()
-        item.listCal()
-        item.listIngredients()
-        } else if (data.status === 0){
-            alert(`Product ${inputVal} not found. Please try another.`)
-        }
+          console.log(data)
+      if (data.status === 1) {
+          const item = new ProductInfo(data.product)
+          item.showInfo()
+          item.listIngredients()
+          item.listNutriments()
+      } else if (data.status === 0) {
+          alert(`Product ${inputVal} not found. Please try another.`)
+      }
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-
-
-  class ProductInfo{
-        constructor(productData){ // I'm passing in data.product
-          this.name = productData.product_name
-          this.ingredients = productData.ingredients
-          this.image = productData.image_url
-          this.nutriments = productData.nutriments
-        }
-
-        testCall(){
-          console.log(this.nutriments.energy)
-        }
-
-        // listCal(){
-        //     for (const entry of Object.entries(this.nutriments)){
-        //         console.log(`${entry[0]} => ${entries[1]}`)
-        //     }
-        // }
-
-
-        showInfo(){
-          document.getElementById('product-image').src = this.image;
-          document.getElementById('product-name').innerText = this.name;
-        }
-
-        listIngredients(){
-          let tableRef = document.getElementById('ingredient-table')
-
-          for (let i = 1; i <tableRef.rows.length;)
-          {
-              tableRef.deleteRow(i);
-          }
-
-          if(!(this.ingredients == null)){
-            for (let key in this.ingredients){
-                let newRow = tableRef.insertRow(-1) //end of the array/row
-                let newICell = newRow.insertCell(0)
-                let newVCell = newRow.insertCell(1)
-                let newIText = document.createTextNode(this.ingredients[key].text)
-                let vegStatus = !(this.ingredients[key].vegetarian) ? 
-                'unknown' : this.ingredients[key].vegetarian 
-                let newVText = document.createTextNode(vegStatus)
-                newICell.appendChild(newIText)
-                newVCell.appendChild(newVText)
-                if (vegStatus === 'no'){
-                    newVCell.classList.add('non-veg-item')
-                }  else if (vegStatus === 'unknown' || vegStatus === 'maybe'){
-                    newVCell.classList.add('unknown-maybe-item')
-                } 
-          }
-        }
-
-
-
-        
-        
-    }
-  }
-
 }
 
+class ProductInfo {
+  constructor(productData) {
+      this.brand = productData.brands
+      this.name = productData.product_name
+      this.ingredients = productData.ingredients
+      this.image = productData.image_url
+      this.nutriments = productData.nutriments
+  }
+
+  showInfo() {
+      console.log(this.ingredients)
+      document.getElementById("product-img").src = this.image;
+      document.getElementById("product-name").innerHTML = `${this.name}`;
+  }
+
+
+  listNutriments() {
+      console.log(this.nutriments)
+      const objNutriments = Object.keys(this.nutriments)
+      const valNutriments = Object.values(this.nutriments)
+      console.log(objNutriments)
+      console.log(valNutriments)
+  }
+
+  listIngredients() {
+      let tableRef = document.getElementById('ingredient-table');
+      for(var i = 1;i<tableRef.rows.length;) {
+          tableRef.deleteRow(i);
+      }
+      if (!(this.ingredients == null)) {
+          for(var key in this.ingredients) {
+              let newRow = tableRef.insertRow(-1);
+              let newICell = newRow.insertCell(0);
+              let newVCell = newRow.insertCell(1);
+              let newIText = document.createTextNode(this.ingredients[key].text);
+              let vegStatus = this.ingredients[key].vegetarian == null ? 'unknown' : this.ingredients[key].vegetarian
+              let newVText = document.createTextNode(vegStatus);
+              newICell.appendChild(newIText);
+              newVCell.appendChild(newVText);
+              if (vegStatus === 'no') {
+                  newVCell.classList.add('non-veg-item')
+              } else if (vegStatus === 'unknown' || vegStatus === 'maybe') {
+                  newVCell.classList.add('unknown-item')
+              }
+          }
+      } 
+  }
+}
